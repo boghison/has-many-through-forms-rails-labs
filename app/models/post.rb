2797dea43e5285bc2bb70db5e765fa5b98b3d489
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-28 15:51:45 +0300

    commit tree 2e6d0c674f73cd0fd9061afb9c8362724d7e9766

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
-
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each {|c| categories << Category.find_or_create_by(name: c[:name]) unless c[:name].blank?}
+  end
 end
